31cfe6793058115898242b1125a0fe7516eafaf4
Nelson Earle <nelson.earle137@gmail.com>
2019-08-28 11:00:01 -0500

    rename module models -> config

--- mucs/mucs.py
+++ mucs/mucs.py
@@ -8,9 +8,9 @@ import subprocess
 from argparse import ArgumentParser
 from subprocess import DEVNULL, PIPE
 
+from config import *
 from consts import *
 from exc import MucsError
-from models import *
 from printer import Printer, printline
 from submit_wrapper import SubmitWrapper
 from util import *
