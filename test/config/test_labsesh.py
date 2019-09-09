6848c126fe7faa6f9cdeff8fb26fa99ec6da2017
Nelson Earle <nelson.earle137@gmail.com>
2019-09-08 19:58:42 -0500

    add tests for LabSesh

---
+++ test/config/test_labsesh.py
@@ -0,0 +1,31 @@
+import datetime
+import unittest
+
+from mucs.config import LabSesh
+from test_utils import *
+
+
+class TestLabSesh(unittest.TestCase):
+
+    def setUp(self):
+        self.labsesh = LabSesh(
+            weekday=NOW.weekday(),
+            start=datetime.time(),
+            end=datetime.time(23, 59, 59, 999999))
+
+    def test_is_active_true(self):
+        self.assertTrue(self.labsesh.is_active())
+
+    def test_is_active_false(self):
+        # If current hour is after noon
+        if NOW.time().hour > 12:
+            # Move end time hour to noon
+            self.labsesh = self.labsesh._replace(
+                end=self.labsesh.end.replace(hour=12))
+        # If current hour is noon or before
+        else:
+            # Move start time to after noon
+            self.labsesh = self.labsesh._replace(
+                start=self.labsesh.start.replace(hour=13))
+
+        self.assertFalse(self.labsesh.is_active())
