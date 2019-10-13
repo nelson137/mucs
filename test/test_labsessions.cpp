799bc520a40e6f69096fbe4f9c32dfa112a8ebc9
Nelson Earle <nelson.earle137@gmail.com>
2019-10-13 16:54:15 -0500

    add tests for labsessions

---
+++ test/test_labsessions.cpp
@@ -0,0 +1,35 @@
+#include "test_labsessions.hpp"
+
+
+TEST_CASE("labsession entry", "[labs]") {
+
+    int ri = rand_int(9);
+    MockCourseConfig mock_config = {
+        {"course_number", ri},
+        {"admin_hash", "!"},
+        {"homeworks", {}},
+        {"roster", {}}
+    };
+
+    SECTION("type is invalid", "[labs][entry]") {
+        string key = "A";
+        mock_config["labs"] = { {key, ri} };
+        REQUIRE_THROWS_WITH(
+            LabSessions(mock_config),
+            "Lab entries must be of type string: " +
+                mock_config.filename + "[\"labs\"][\"" + key + "\"]"
+        );
+    }
+
+    SECTION("type is valid", "[labs][entry]") {
+        string key = "A";
+        mock_config["labs"] = { {key, ""} };
+        try {
+            LabSessions(mock_config);
+            SUCCEED("Successfully created LabSessions object");
+        } catch (mucs_exception& me) {
+            FAIL(me.what());
+        }
+    }
+
+}
---
+++ test/test_labsessions.hpp
@@ -0,0 +1,19 @@
+#ifndef TEST_LABSESSIONS_HPP
+#define TEST_LABSESSIONS_HPP
+
+
+#include <string>
+
+#include "catch.hpp"
+
+#include "mucs/except.hpp"
+
+#include "test_util.hpp"
+
+#include "../src/courseconfig.hpp"
+#include "../src/labsessions.hpp"
+
+using namespace std;
+
+
+#endif
