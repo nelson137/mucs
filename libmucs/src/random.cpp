8a3fd28bfaf0f828f1024cab2fd92be42cec8188
Nelson Earle <nelson.earle137@gmail.com>
2020-02-12 22:19:41 -0600

    fix error initializing variable-sized obj

--- libmucs/src/random.cpp
+++ libmucs/src/random.cpp
@@ -49,7 +49,7 @@ char rand_char(const chars_t& chars) {
 
 
 string rand_string(const int size, const chars_t& chars) {
-    char s[size+1] = {0};
+    char s[size+1];
     for (int i=0; i<size; i++)
         s[i] = rand_char(chars);
     return s;
