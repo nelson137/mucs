6012b81a921879464b37756e9d5204b0686cf20f
Nelson Earle <nelson.earle137@gmail.com>
2020-02-15 21:40:14 -0600

    add tests for stl_contains() and stl_join()

--- test/test_util.cpp
+++ test/test_util.cpp
@@ -147,3 +147,57 @@ TEST_CASE("parse_weekday", "[parse_weekday]") {
         );
     }
 }
+
+
+TEST_CASE("stl_contains", "[stl_contains]") {
+    vector<int> container;
+    int target = rand_int(10, 20);
+
+    SECTION("empty container") {
+        REQUIRE(stl_contains(container, target) == false);
+    }
+
+    SECTION("non-empty container") {
+        int n_before_target = rand_int(5);
+        int n_after_target = rand_int(5);
+        container.reserve(n_before_target + 1 + n_after_target);
+
+        for (int i=0; i<n_before_target; i++)
+            container.push_back(rand_int(5));
+        container.push_back(target);
+        for (int i=0; i<n_after_target; i++)
+            container.push_back(rand_int(5));
+
+        SECTION("valid") {
+            REQUIRE(stl_contains(container, target) == true);
+        }
+
+        SECTION("invalid") {
+            container.erase(container.begin() + n_before_target);
+            REQUIRE(stl_contains(container, target) == false);
+        }
+    }
+}
+
+
+TEST_CASE("stl_join", "[stl_join]") {
+    vector<int> container;
+    string expected;
+
+    SECTION("empty container") {
+        string actual = stl_join(container);
+    }
+
+    SECTION("non-empty container") {
+        SECTION("with 1 element") {
+            container = {1};
+            expected = "1";
+        }
+        SECTION("with multiple element") {
+            container = {1, 2, 3};
+            expected = "1,2,3";
+        }
+    }
+
+    REQUIRE_THAT(stl_join(container), Equals(expected));
+}
