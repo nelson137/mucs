1be2025fe9fdd372d3b748390be23b91521d4d0c
Nelson Earle <nelson.earle137@gmail.com>
2020-09-08 23:24:36 -0500

    add tests for mucs_exception

---
+++ test/test_except.hpp
@@ -0,0 +1,17 @@
+#ifndef TEST_EXCEPT_HPP
+#define TEST_EXCEPT_HPP
+
+
+#include <sstream>
+#include <string>
+
+#include "catch.hpp"
+
+#include "random.hpp"
+
+#include "../src/except.hpp"
+
+using namespace std;
+
+
+#endif
